59b401cd5e5db05ff95a2855871bc53498abf2dd
cpr122 <160140454+cpr122@users.noreply.github.com>
2024-03-06 19:50:34 -0600

    Create arregloD.c

---
+++ arregloD.c
@@ -0,0 +1,12 @@
+int main(){
+    int arr [10];
+    int contador = 0;
+    while (contador < 10)
+    {
+        arr[contador] = 10;
+        printf("%d\n", arr[contador]);
+        contador++;
+    }
+    
+    return 0;
+}
